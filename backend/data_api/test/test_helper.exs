93d2412b082053ec3b39ca7a9c6082a041bfa441
Alena <elen.malets@gmail.com>
2025-07-02 09:46:16 +0200

    Fix test: preload Redis schema for /schema/users route

--- backend/data_api/test/test_helper.exs
+++ backend/data_api/test/test_helper.exs
@@ -1,1 +1,7 @@
 ExUnit.start()
+
+# Load Redis schema for test
+{:ok, _} = Redix.start_link(name: :redis_server)
+
+schema = %{"table" => "users", "columns" => ["name", "email", "role"]}
+Redix.command!(:redis_server, ["SET", "compiled_schema:users", Jason.encode!(schema)])
